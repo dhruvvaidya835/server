311ef6d4efb6d1e1b0373d28ffd396e7d1cc9fcc
Dhruv83 <dhruvvaidya835@gmail.com>
2025-08-03 22:25:06 +0530

    go

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,7 @@ const User = require('../models/userModel');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 
+
 // Generate JWT
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
